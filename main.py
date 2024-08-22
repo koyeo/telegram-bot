60f2b33acb07167c7813fb66eaf66e801adee967
koyeo <cansen.hu@certik.com>
2024-08-22 15:46:56 +0800

    fix: update requirements

--- main.py
+++ main.py
@@ -7,12 +7,19 @@ from src.bot.message_handler import handle_message, handle_reply, export_csv
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 import logging
 
+from src.data.docsend_extract import extract_docsend_content
+
+
 def add_handlers(application: Application):
     logging.info("Adding handlers")
     application.add_handler(MessageHandler(filters.REPLY & filters.TEXT, handle_reply))
-    application.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, handle_message))
+    application.add_handler(MessageHandler(filters.ALL & filters.COMMAND, handle_message))
     application.add_handler(CommandHandler('export_csv', export_csv))
 async def main():
+    # combined_text, pdf_paths = await extract_docsend_content('https://docsend.com/view/6e6u6jhng897d5v5','lam92007@gmail.com','')
+    # print('combined_text',combined_text)
+    # print('pdf_paths',pdf_paths)
+
     bot = setup_bot()
     application = ApplicationBuilder().token(bot.token).build()
     add_handlers(application)
